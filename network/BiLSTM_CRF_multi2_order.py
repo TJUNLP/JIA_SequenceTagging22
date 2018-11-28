32322bf5870b86d249858823d58abf34e9b2a17e
TJUNLP <jiashengbin0501@163.com>
2018-11-28 18:04:21 +0800

    new34

--- network/BiLSTM_CRF_multi2_order.py
+++ network/BiLSTM_CRF_multi2_order.py
@@ -643,10 +643,6 @@ def BiLSTM_CRF_multi2_order_pos(sourcevocabsize, targetvocabsize, source_W, inpu
     mlp2_hidden1 = Bidirectional(LSTM(hidden_dim, return_sequences=True), merge_mode='concat')(BiLSTM_dropout)
     mlp2_hidden1 = Dropout(0.5)(mlp2_hidden1)
 
-    BiLSTM_pos = Bidirectional(LSTM(hidden_dim, return_sequences=True), merge_mode='concat')(pos_embeding)
-    BiLSTM_pos = Dropout(0.5)(BiLSTM_pos)
-    mlp2_hidden1 = concatenate([BiLSTM_pos, mlp2_hidden1], axis=-1)
-
     mlp2_hidden2 = TimeDistributed(Dense(100, activation='tanh'))(mlp2_hidden1)
     mlp2_hidden3 = concatenate([mlp1_hidden2, mlp2_hidden2], axis=-1)
     mlp2_hidden3 = TimeDistributed(Dense(100, activation='tanh'))(mlp2_hidden3)
