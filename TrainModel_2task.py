24ce0554585dbac738bcf4f9f37a84fa4e3f1628
TJUNLP <jiashengbin0501@163.com>
2019-01-08 10:12:10 +0800

    1-18-1

--- TrainModel_2task.py
+++ TrainModel_2task.py
@@ -29,9 +29,9 @@ from keras.layers.normalization import BatchNormalization
 from keras.callbacks import Callback
 # from keras.losses import my_cross_entropy_withWeight
 from network.BiLSTM_CRF_multi2_alone import BiLSTM_CRF_multi2_alone
-from network.BiLSTM_CRF_multi2_order import BiLSTM_CRF_multi2_order, BiLSTM_CRF_multi2_order2, BiLSTM_CRF_multi2_order3, BiLSTM_CRF_multi2_order31
-from network.BiLSTM_CRF_multi2_order import BiLSTM_CRF_multi2_order3_split, BiLSTM_CRF_multi2_order3_Dense
-
+from network.BiLSTM_CRF_multi2_order import BiLSTM_CRF_multi2_order, BiLSTM_CRF_multi2_order2, BiLSTM_CRF_multi2_order3
+from network.BiLSTM_CRF_multi2_order import BiLSTM_CRF_multi2_order3_Dense
+from network.BiLSTM_CRF_multi2_order import BiLSTM_CRF_multi2_order3_full
 
 
 def test_model(nn_model, testdata, chardata, pos_data, index2word, resultfile='', batch_size=50):
@@ -171,8 +171,8 @@ def SelectModel(modelname, sourcevocabsize, targetvocabsize, source_W,
                                               char_emd_dim=char_emd_dim,
                                               sourcepossize=sourcepossize, pos_W=pos_W, pos_emd_dim=pos_emd_dim)
 
-    elif modelname is 'BiLSTM_CRF_multi2_order31':
-        nn_model = BiLSTM_CRF_multi2_order31(sourcevocabsize=sourcevocabsize, targetvocabsize=targetvocabsize,
+    elif modelname is 'BiLSTM_CRF_multi2_order3_full':
+        nn_model = BiLSTM_CRF_multi2_order3_full(sourcevocabsize=sourcevocabsize, targetvocabsize=targetvocabsize,
                                               source_W=source_W,
                                               input_seq_lenth=input_seq_lenth,
                                               output_seq_lenth=output_seq_lenth,
@@ -381,6 +381,7 @@ if __name__ == "__main__":
     modelname = 'BiLSTM_CRF_multi2_order2'
     modelname = 'BiLSTM_CRF_multi2_order3'
     # modelname = 'BiLSTM_CRF_multi2_order3_Dense'
+    modelname = 'BiLSTM_CRF_multi2_order3_full'
 
 
 
@@ -393,7 +394,9 @@ if __name__ == "__main__":
 
     datafile = "./model/data_fix=" + str(withFix) + "_pos=" + str(withPos) + ".pkl"
 
-    modelfile = "./model/" + modelname + "__" + "data_fix=" + str(withFix) + "_pos=" + str(withPos) + "_classweight(1-10)_1.h5"
+    # modelfile = "./model/" + modelname + "__" + "data_fix=" + str(withFix) + "_pos=" + str(withPos) + "_classweight(1-10)_1.h5"
+    modelfile = "./model/" + modelname + "__" + "data_fix=" + str(withFix) + "_pos=" + str(
+        withPos) + "_1.h5"
 
     resultdir = "./data/result/"
 
