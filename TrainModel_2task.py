8d8f6daf120d2c5ab8b4051c925ff128b4218dcf
TJUNLP <jiashengbin0501@163.com>
2019-01-12 18:56:13 +0800

    1-12-1

--- TrainModel_2task.py
+++ TrainModel_2task.py
@@ -395,7 +395,7 @@ if __name__ == "__main__":
     modelname = 'BiLSTM_CRF_multi2_order3'
     # modelname = 'BiLSTM_CRF_multi2_order3_Dense'
     modelname = 'BiLSTM_CRF_multi2_order3_DenseAvg'
-    modelname = 'BiLSTM_CRF_multi2_order3_Coor'
+    # modelname = 'BiLSTM_CRF_multi2_order3_Coor'
 
 
 
