affe130d583ac42aac6a85981f075859625c52fe
TJUNLP <jiashengbin0501@163.com>
2019-03-16 11:21:06 +0800

    3-16-3

--- ProcessData_S2F.py
+++ ProcessData_S2F.py
@@ -206,7 +206,8 @@ def make_idx_word_index(fraglist, max_context, max_fragment):
 
         data_fragment = fragment + [0] * max(0, max_fragment-len(fragment))
         data_t = np.zeros(4)
-        data_t[word2index_Type[fragment_tag]] = 1
+        if fragment_tag in word2index_Type.keys():
+            data_t[word2index_Type[fragment_tag]] = 1
 
         data_leftcontext = [0] * max(0, max_context-len(context_left)) + context_left
         data_rightcontext = context_right + [0] * max(0, max_context-len(context_right))
