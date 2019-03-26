0225eeaf48467bb030e8e1f553eae3c30af4a776
TJUNLP <jiashengbin0501@163.com>
2019-03-26 09:50:41 +0800

    3-26-1

--- TrainModel_simul.py
+++ TrainModel_simul.py
@@ -256,6 +256,8 @@ if __name__ == "__main__":
 
         get_data(trainfile,devfile, testfile, w2v_file, c2v_file, datafile,
                  w2v_k=100, c2v_k=50, maxlen=maxlen, hasNeg=hasNeg)
+    else:
+        print(datafile, "is exited ... loading ....")
 
     traindata, devdata, testdata,\
     chartrain, chardev, chartest,\
